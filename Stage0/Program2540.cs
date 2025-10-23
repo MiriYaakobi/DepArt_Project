872ba54bb515f65e97d79783e62418bcbc6c8076
OdelyaGil <ogil@g.jct.ac.il>
2025-10-23 22:04:07 +0300

    feat: finalize stage 0 environment setup

---
+++ Stage0/Program2540.cs
@@ -0,0 +1,20 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Welcome2540();
+            Welcome1617();
+            Console.ReadKey();
+        }
+        static partial void Welcome1617();
+        private static void Welcome2540()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, wlcome to my first console application", name);
+        }
+    }
+}
