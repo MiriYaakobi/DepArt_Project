91dda5e78b3294e4cb60fdad3660f1bbf1809566
OdelyaGil <ogil@g.jct.ac.il>
2025-10-23 22:04:07 +0300

    Initial Code + Welcome function

--- Stage0/Program2540.cs
+++ Stage0/Program2540.cs
@@ -1,17 +1,20 @@
-﻿partial class Program
+﻿namespace Stage0
 {
-    private static void Main(string[] args)
+    partial class Program
     {
-        Console.WriteLine("Hello, World!");
-        Welcome2540();
-        Welcome1617();
-        Console.ReadKey();
-    }
-   static partial void Welcome1617();
-    private static void Welcome2540()
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("{0}, wlcome to my first console application", name);
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Welcome2540();
+            Welcome1617();
+            Console.ReadKey();
+        }
+        static partial void Welcome1617();
+        private static void Welcome2540()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, wlcome to my first console application", name);
+        }
     }
 }
