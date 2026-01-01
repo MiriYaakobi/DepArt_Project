3e4304dc3d3da472f1ca0e65e84d9ef895b304b0
OdelyaGil <ogil@g.jct.ac.il>
2026-01-01 11:11:10 +0200

    refactoring

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Globalization;
-using System.Windows; // חובה בשביל Visibility
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 
-namespace PL.Converters;
+namespace PL;
+
+public class StatusToColorConverter : IValueConverter
+{
+    // המרה מהנתונים (האם פעיל?) לצבע (Brush)
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // אנחנו מצפים לקבל בוליאני (true/false)
+        if (value is bool isActive)
+        {
+            if (!isActive) // אם לא פעיל
+            {
+                // מחזירים צבע אדמדם/אפרפר בהיר לסימון
+                return Brushes.LavenderBlush;
+            }
+        }
+
+        // אם פעיל - צבע רגיל (שקוף/לבן)
+        return Brushes.Transparent;
+    }
+
+    // המרה הפוכה (לא רלוונטי לצבעים, לכן זורקים שגיאה)
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 // ממיר 1: טקסט לכפתור
 public class IdToContentConverter : IValueConverter
